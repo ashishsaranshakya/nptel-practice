bff7bcac6280d94e3b9e5057fee28ce798348c9e
Ashish Saran Shakya <harshshakya18585@gmail.com>
2024-09-03 21:33:24 +0530

    add light/dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import quizData from './assets/questions.json';
+import lightIcon from './assets/light.svg';
+import darkIcon from './assets/dark.svg';
 
 const shuffleArray = (array) => {
   return array.sort(() => Math.random() - 0.5);
@@ -12,8 +14,16 @@ const App = () => {
   const [selectedOption, setSelectedOption] = useState('');
   const [score, setScore] = useState(0);
   const [isFinished, setIsFinished] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
+    const savedTheme = localStorage.getItem('darkMode');
+    if (savedTheme) {
+      const isDark = JSON.parse(savedTheme);
+      setIsDarkMode(isDark);
+      document.body.classList.toggle('dark-mode', isDark);
+    }
+
     const shuffledQuestions = quizData.map((question) => {
       return {
         ...question,
@@ -40,8 +50,25 @@ const App = () => {
     }
   };
 
+  const toggleDarkMode = () => {
+    const newMode = !isDarkMode;
+    setIsDarkMode(newMode);
+    document.body.classList.toggle('dark-mode', newMode);
+    localStorage.setItem('darkMode', JSON.stringify(newMode));
+  };
+
   return (
     <div className='container'>
+      {
+        isDarkMode ? 
+          (<button className='dark-mode-toggle-dark' onClick={toggleDarkMode}>
+            <img src={lightIcon} alt="Light Mode" />
+          </button>):
+          (<button className='dark-mode-toggle' onClick={toggleDarkMode}>
+            <img src={darkIcon} alt="Dark Mode" />
+          </button>)
+      }
+      
       {isFinished ? (
         <div>
           <h1>Quiz Finished!</h1>
